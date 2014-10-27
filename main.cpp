300308c33ca1add6d45817e979164ac6781daa68
zakia00 <ks2190@msstate.edu>
2014-10-27 11:36:06 -0700

    display-item-zakia

--- main.cpp
+++ main.cpp
@@ -10,5 +10,17 @@ int main()
 
 menu(){
 	cout<<"What you want to do?\n"
-		cout<<"1. List Items\n2. Bid Items\n"
+	cout<<"1. List Items\n2. Bid Items\n"
 	}
+
+Display(){
+	for(int i=0;i<itemIndex;i++)
+	{
+		cout<<"Item %d information:"<<i;
+		cout<<"Item Id: "<<items[i].getID()<<"\n";
+		cout<<"Item Name: "<<items[i].getName()<<"\n";
+		cout<<"Item highestPrice: "<<items[i].getHighestPrice()<<"\n";
+		cout<<"Item highestBidder: "<<items[i].getHighestBidder()<<"\n";
+
+	}
+}
