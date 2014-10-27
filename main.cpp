ff690abb9c69784332b06f549f9738ed73626991
nafiseh-ss <ns553@msstate.edu>
2014-10-27 11:32:41 -0500

    menu
    add

--- main.cpp
+++ main.cpp
@@ -5,11 +5,9 @@ using namespace std;
 
 int main()
 {
-    def menu
-	
-}
 
 menu(){
 	cout<<"What you want to do?\n"
     cout<<"1. List Items\n2. Bid Items\n3. Add Buyers\n4. Add Item\n5. Display Transaction\n6. Exit"
 	}
+}
