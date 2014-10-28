e6575cb5feb9cc83fc543695a3e0e5736358b17e
nafiseh-ss <ns553@msstate.edu>
2014-10-28 14:29:22 -0500

    buyer
    improve

--- Buyer.cpp
+++ Buyer.cpp
@@ -4,7 +4,7 @@ class Buyer {
 		string BuyerName;
 	public:
 		int getBuyerID() { return id; }
-        string getBuyerName() {return name;}
-        setId(int tid) {id=tid;} // not sure about it!
-        setName(String tname) {name=tname;} // not sure about it!!
-}
+        	string getBuyerName() {return name;}
+        	setId(int tid) {id=tid;} // not sure about it!
+        	setName(String tname) {name=tname;} // not sure about it!!
+}	
