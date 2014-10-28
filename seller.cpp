c148e3953b5c13f4c0221189b3ab916136663cb7
Will Harrison <jwh335@gmail.com>
2014-10-28 16:37:00 -0500

    added library and std

--- seller.cpp
+++ seller.cpp
@@ -1,15 +1,17 @@
+#include<string>
+
 class Seller { 
-	private:
-		int SellerID;
-		string SellerName;
-	public:
-		void setID(int tid){SellerID=tid;}
-		int getID() { return SellerID; }
-		string getName() { return SellerName; }
-		void setName(string tname){SellerName=tname;}
-		
-		//Constructor
-		Seller(){};
-		void setData(int _SellerID, string _SellerName) { SellerID = _SellerID; SellerName = _SellerName; }
-		
+    private:
+        int SellerID;
+        std::string SellerName;
+    public:
+        void setID(int tid){SellerID=tid;}
+        int getID() { return SellerID; }
+        std::string getName() { return SellerName; }
+        void setName(std::string tname){SellerName=tname;}
+        
+        //Constructor
+        Seller(){};
+        void setData(int _SellerID, std::string _SellerName) { SellerID = _SellerID; SellerName = _SellerName; }
+        
 };
