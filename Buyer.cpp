b49d8b80e24dd0182048655d1f93101f6711fd66
Will Harrison <jwh335@gmail.com>
2014-10-28 16:19:49 -0500

    fixed tabs vs spaces

--- Buyer.cpp
+++ Buyer.cpp
@@ -1,15 +1,15 @@
 #include<string>
 
 class Buyer {
-	private:
-		int BuyerID;
+    private:
+        int BuyerID;
         std::string BuyerName;
-	public:
-		int getBuyerID() { return BuyerID; }
+    public:
+        int getBuyerID() { return BuyerID; }
         std::string getBuyerName() {return BuyerName;}
-		void setId(int tid) {BuyerID=tid;} // not sure about it!
-		void setName(std::string tname) {BuyerName=tname;} // not sure about it!!
-		
-		Buyer(){};
-		void setData(int id, std::string name){BuyerID=id; BuyerName=name;}
+        void setId(int tid) {BuyerID=tid;} // not sure about it!
+        void setName(std::string tname) {BuyerName=tname;} // not sure about it!!
+        
+        Buyer(){};
+        void setData(int id, std::string name){BuyerID=id; BuyerName=name;}
 };
