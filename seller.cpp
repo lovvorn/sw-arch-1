5eb553b50c62e344a0bf8f273712b2d6d8b6784f
Will Harrison <jwh335@gmail.com>
2014-10-28 16:05:34 -0500

    added library and namespace

--- seller.cpp
+++ seller.cpp
@@ -1,15 +1,17 @@
+#include<string>
+
 class Seller { 
 	private:
 		int SellerID;
-		string SellerName;
+        std::string SellerName;
 	public:
 		void setID(int tid){SellerID=tid;}
 		int getID() { return SellerID; }
-		string getName() { return SellerName; }
-		void setName(string tname){SellerName=tname;}
+        std::string getName() { return SellerName; }
+		void setName(std::string tname){SellerName=tname;}
 		
 		//Constructor
 		Seller();
-		void setData(int, string);
+		void setData(int, std::string);
 		
 };
