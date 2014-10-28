636f1fd7592d68423d82d4daf87f8018abf697b0
Will Harrison <jwh335@gmail.com>
2014-10-28 16:17:40 -0500

    added library and std

--- Buyer.cpp
+++ Buyer.cpp
@@ -1,13 +1,15 @@
+#include<string>
+
 class Buyer {
 	private:
 		int BuyerID;
-		string BuyerName;
+        std::string BuyerName;
 	public:
 		int getBuyerID() { return BuyerID; }
-		string getBuyerName() {return BuyerName;}
+        std::string getBuyerName() {return BuyerName;}
 		void setId(int tid) {BuyerID=tid;} // not sure about it!
-		void setName(string tname) {BuyerName=tname;} // not sure about it!!
+		void setName(std::string tname) {BuyerName=tname;} // not sure about it!!
 		
 		Buyer(){};
-		void setData(int id, string name){BuyerID=id; BuyerName=name;}
+		void setData(int id, std::string name){BuyerID=id; BuyerName=name;}
 };
