4865d1309d9cd2fe1ae8bd1017bb5b4ad75be111
Will Harrison <jwh335@gmail.com>
2014-10-28 16:08:18 -0500

    removed spaces in favor of tabs

--- seller.cpp
+++ seller.cpp
@@ -3,11 +3,11 @@
 class Seller { 
 	private:
 		int SellerID;
-        std::string SellerName;
+		std::string SellerName;
 	public:
 		void setID(int tid){SellerID=tid;}
 		int getID() { return SellerID; }
-        std::string getName() { return SellerName; }
+		std::string getName() { return SellerName; }
 		void setName(std::string tname){SellerName=tname;}
 		
 		//Constructor
