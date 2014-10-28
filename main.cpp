53a39fa4c513985e8d81e4bd0da0660528bc620b
Hunter Lovvorn <hunter.lovvorn@me.com>
2014-10-28 15:11:00 -0500

    Function type
    Added function declaration type

--- main.cpp
+++ main.cpp
@@ -112,12 +112,12 @@ void Bid()
 	cout<<"\nbid\n";
 }
 
-menu(){
-		cout<<"1. List Items\n2. Bid Items\n3. Add Buyers\n4. Add Item\n5. Display Transaction\n6. Exit\nChoice: "
+void menu(){
+		cout<<"1. List Items\n2. Bid Items\n3. Add Buyers\n4. Add Item\n5. Display Transaction\n6. Exit\nChoice: ";
         cin>>choice;
         if(choice==1){List_Items();}
         else if (choice==2) {Bid();}
-        else if (choice==3) {Add_Buyer()}
+        else if (choice==3) {Add_Buyer();}
         else if (choice==4) {Add_Item();}
         else if (choice==5) {Display_Trans();}
         else if (choice==6) {End_Bidding();}
