bfd863e7b4856c6f6ea2ed60d54015b87899b009
Will Harrison <jwh335@gmail.com>
2014-10-29 09:54:13 -0500

    cleaning up

--- Bid.cpp
+++ Bid.cpp
@@ -1,12 +1,27 @@
 class Bid{
+
 	private: 
+
 		int BidID;
 		int BuyerID;
 		int ItemID;
+
 		double proposed_price;
+
 	public:
-		Bid(){}
-		void setData(int biid, int bid, int iid, double price){BidID = biid; BuyerID=bid; ItemID=iid; proposed_price=price;}
+
+		Bid() {}
+
 		int getID() { return BidID; }
-		void setID(int bid){BidID=bid;}
+
+		void setData(int biid, int bid, int iid, double price)
+		{
+			BidID = biid;
+			BuyerID = bid;
+			ItemID = iid;
+			proposed_price = price;
+		}
+
+		void setID(int bid) { BidID = bid; }
+
 };
--- Buyer.cpp
+++ Buyer.cpp
@@ -1,15 +1,28 @@
 #include<string>
 
 class Buyer {
+
 	private:
+
 		int BuyerID;
+
 		std::string BuyerName;
+
 	public:
-		int getBuyerID() { return BuyerID; }
-		std::string getBuyerName() {return BuyerName;}
-		void setId(int tid) {BuyerID=tid;} // not sure about it!
-		void setName(std::string tname) {BuyerName=tname;} // not sure about it!!
-		
+
 		Buyer(){};
-		void setData(int id, std::string name){BuyerID=id; BuyerName=name;}
+
+		int getBuyerID() { return BuyerID; }
+
+		std::string getBuyerName() {return BuyerName;}
+
+		void setId(int tid) { BuyerID = tid; } // not sure about it!
+		void setName(std::string tname) { BuyerName = tname; } // not sure about it!!
+
+		void setData(int id, std::string name)
+		{
+			BuyerID = id;
+			BuyerName = name;
+		}
+
 };
