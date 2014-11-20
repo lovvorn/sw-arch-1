aed32ebc80fe9edcef2f438f6a3c86751b407d68
willharrison <jwh335@gmail.com>
2014-11-20 15:29:16 -0600

    method definitions

--- python/model.py
+++ python/model.py
@@ -4,3 +4,27 @@ class Model:
 
     def __init__(self):
         pass
+
+    def setSymbol(self, symbol):
+        self.symbol = symbol
+
+    def getDescription(self):
+        pass
+
+    def getClosingPrice(self):
+        pass
+
+    def getDailyNetChange(self):
+        pass
+
+    def getVolume(self):
+        pass
+
+    def getAverageVolume(self):
+        pass
+
+    def getHigh(self):
+        pass
+
+    def getLow(self):
+        pass
